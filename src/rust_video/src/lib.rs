4db6c5efbad0671e74c6c23988ac5e7c126f3e2e
Alwin Stockinger <alwin.stockinger@tum.de>
2021-06-01 12:20:54 +0200

    Get Video Info implementation

--- src/rust_video/src/lib.rs
+++ src/rust_video/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::storage;
+use ic_cdk_macros::*;
+use std::collections::HashMap;
+
+type VideoStore = HashMap<VideoId, VideoInfo>;
+type VideoId = String;
+type VideoChunks = [u8];
+type ChunkStore = HashMap<VideoId, VideoChunks>; 
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize)]
+struct VideoInfo{
+    pub videoId: VideoId,
+    pub name: String,
+    pub description: String,
+    pub keywords: Vec<String>,
+}
+
+#[query]
+fn get_video_info(id: VideoId) -> VideoInfo {
+    let video_store = storage::get::<VideoStore>();
+
+    video_store
+        .get(&id)
+        .cloned()
+        .unwrap_or_else(|| VideoInfo::default())
 }
