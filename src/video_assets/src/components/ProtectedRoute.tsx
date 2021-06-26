843918b5ec66d757bbc4039d50c36a33e06bf544
Moritz Schindelmann <moritz.schindelmann@maibornwolff.de>
2021-06-26 23:19:32 +0200

    fix(auth): frontend cleanup

--- src/video_assets/src/components/ProtectedRoute.tsx
+++ src/video_assets/src/components/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 
-const ProtectedRoute = ({ component: Component, ...rest }: any) => {
+const ProtectedRoute = ({ component: Component, ...rest }: RouteProps) => {
   const { isLoading, isAuthenticated } = useContext(AuthContext);
 
   if (isLoading) {
