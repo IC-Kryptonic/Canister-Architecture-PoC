f16d98111fe97a32ddaa554a10957b6536434ce9
Alwin Stockinger <alwin.stockinger@tum.de>
2021-07-12 11:37:44 +0200

    test: inter canister calls

--- src/video_dht/src/lib.rs
+++ src/video_dht/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use ic_cdk::export::candid::{CandidType, Deserialize, Principal};
+use ic_cdk_macros::*;
+use ic_cdk::api::call;
+
+#[update(name = "testBucket")]
+pub async fn call_bucket() {
+    let principal = Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap();
+    let _res: Result<(i32,), _>  = call::call( principal, "test", ()).await;
 }
