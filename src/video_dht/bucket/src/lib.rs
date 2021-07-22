2e16987d0a0043b7e43fd105ab43e514776db4c4
Alwin Stockinger <alwin.stockinger@tum.de>
2021-07-22 15:25:02 +0200

    feat: insert chunk method for bucket

--- src/video_dht/bucket/src/lib.rs
+++ src/video_dht/bucket/src/lib.rs
@@ -15,4 +15,18 @@ pub async fn create_video(id: VideoId, chunk_num: ChunkNum){
     let video_storage = storage::get_mut::<VideoStore>();
 
     video_storage.insert(id, vec![Vec::new(); chunk_num]);
+    ic_cdk::print("Created a new video");
+}
+
+///Inserts new chunk into existing video
+#[update(name = "insertChunk")]
+pub async fn insert_chunk(id: VideoId, chunk_num: ChunkNum, chunk: Chunk){
+    let video_storage = storage::get_mut::<VideoStore>();
+
+    match video_storage.get_mut(&id){
+        Some(chunks) => chunks[chunk_num] = chunk,
+        None => {
+            ic_cdk::api::trap("that chunk number does not exist for the video");
+        }
+    }
 }
