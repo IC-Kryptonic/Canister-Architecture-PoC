9502ce499156aa6e44738d32b0f971d73d69d7a6
Stefan Vladov <svladov.dev@gmail.com>
2021-07-31 16:50:36 +0200

    add quick start script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Requires a cloned version of https://github.com/dfinity/internet-identity
+export IDENTITY_PATH=../internet-identity
+
+# # Update identity
+pushd $IDENTITY_PATH
+git clean -df # we assume we make no changes to internet identity repo
+git checkout -- . # so we discard any changes, e.g package-lock.json
+git pull
+npm install
+popd
+
+# Update current repository
+npm install
