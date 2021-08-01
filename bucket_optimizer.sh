9a8959f852ea913d831885dfd30175f22b34ef10
Alwin Stockinger <alwin.stockinger@tum.de>
2021-08-01 09:22:09 +0200

    chore: interface test clean up

--- bucket_optimizer.sh
+++
@@ -1,1 +0,0 @@
-ic-cdk-optimizer target/wasm32-unknown-unknown/release/bucket.wasm -o target/wasm32-unknown-unknown/release/bucket_opt.wasm
--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ echo "Running testing script..."
 echo "Starting replica"
 dfx start --background --clean
 
+echo "Compiling and optimizing bucket code"
+./bucket_creation.sh
 echo "Deploying backend canister"
 dfx deploy backend
 
@@ -25,5 +27,14 @@ dfx canister call backend getProfile '(principal "aaaaa-aa")'
 dfx canister call backend getCurrentProfile
 dfx canister call backend createProfile '(record {"principal" = principal "aaaaa-aa"; name = "testuser" ; likes = vec {}})'
 dfx canister call backend likeVideo '("videoId")'
+
+
+dfx canister call video_dht createVideo '("id", 1)'
+dfx canister call video_dht insertChunk '("id", 0, blob "\CA\FF\EE")'
+dfx canister call video_dht getChunk '("id", 0)'
+dfx canister call video_dht getBucketPrincipal '("id")'
+
+dfx canister call backend createVideo '(record {video_id = null; owner = principal "aaaaa-aa"; name = "video1"; description = "mountain dog"; keywords = vec {"scars"; "toast"}; storage_type = variant {simpleDistMap = record {1; null}}})'
+
 
 dfx stop
