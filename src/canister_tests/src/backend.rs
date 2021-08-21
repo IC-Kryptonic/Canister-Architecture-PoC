4ca6fd3bf808fe3948b5a6141f15df3ea260b270
Alwin Stockinger <alwin.stockinger@tum.de>
2021-08-21 12:18:12 +0200

    send and receive objects

--- src/canister_tests/src/backend.rs
+++ src/canister_tests/src/backend.rs
@@ -1,15 +1,18 @@
 use ic_agent::{Agent};
-/*use ic_cdk::export::candid::{Encode, Decode, CandidType, Nat};
-use serde::Deserialize;*/
 use ic_agent::export::Principal;
-/*use ic_cdk_macros::*;
-use ic_cdk;
-*/
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::export::candid::{Encode, Decode};
+
 use crate::util;
 
 
-/*
 pub type VideoId = String;
+pub type ChunkNum = usize;
+
+#[derive(Clone, CandidType, Deserialize)]
+pub struct IPFSData{
+    data: String,
+}
 
 #[derive(CandidType, Deserialize)]
 pub enum StorageType{
@@ -29,8 +32,8 @@ pub struct VideoInfo{
     pub name: String,
     pub description: String,
     pub keywords: Vec<String>,
-    pub storage_type: video_storage::StorageType,
-}*/
+    pub storage_type: StorageType,
+}
 
 
 pub async fn test_backend() -> bool{
@@ -58,7 +61,7 @@ pub async fn test_backend() -> bool{
 
 async fn test_create_video(agent: Agent, princ: Principal) -> bool{
 
-    /*let storage_type = StorageType::InCanister(1);
+    let storage_type = StorageType::InCanister(1);
 
     let video_info = VideoInfo{
         video_id: None,
@@ -68,7 +71,7 @@ async fn test_create_video(agent: Agent, princ: Principal) -> bool{
         description: "testing_desc".to_string(),
         keywords: vec!["testing_keyword1".to_string(), "testing_keyword2".to_string()],
         storage_type,
-    };*/
+    };
 
     let waiter = garcon::Delay::builder()
         .throttle(std::time::Duration::from_millis(500))
@@ -76,16 +79,21 @@ async fn test_create_video(agent: Agent, princ: Principal) -> bool{
         .build();
 
     let response = agent.update(&princ, "createVideo")
-        //.with_arg(&candid::Encode!(&video_info))
+        .with_arg(&Encode!(&video_info).expect("Could not encode"))
         .call_and_wait(waiter)
         .await;
 
 
-    return match response {
-        Ok(_result) => true,
+    let result = match response {
+        Ok(result) => Decode!(result.as_slice(), VideoInfo).expect("Could not deduce video info from result"),
         Err(err) => {
             println!("Api Call error: {:?}", err);
             return false;
         },
-    }
+    };
+
+
+    println!("Video: {:?}", result.video_id.clone().unwrap());
+
+    return result.video_id.is_some();
 }
