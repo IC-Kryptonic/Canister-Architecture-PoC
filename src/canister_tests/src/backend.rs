02cb73fd5d44b6b742a091b73df9c50e6478c5b5
Alwin Stockinger <alwin.stockinger@tum.de>
2021-08-22 12:52:28 +0200

    getVideoInfo test

--- src/canister_tests/src/backend.rs
+++ src/canister_tests/src/backend.rs
@@ -55,16 +55,63 @@ pub struct VideoInfo{
     pub storage_type: StorageType,
 }
 
+pub type WrappedVideoInfo = Option<VideoInfo>;
 
 pub async fn test_backend(agent: &Agent) -> bool{
 
     let actor = Actor::from_name(agent, "backend");
 
-    if !test_create_video(&actor).await{
+    if test_create_video(&actor).await && test_create_and_get_video_info(&actor).await{
+        println!("All backend tests successful ✅");
+        return true;
+    } else {
         return false;
     }
+}
 
-    println!("All backend tests successful ✅");
+async fn test_create_and_get_video_info(actor: &Actor<'_>) -> bool{
+    let storage_type = StorageType::InCanister(1);
+
+    let video_info = VideoInfo{
+        video_id: None,
+        owner: Principal::from_slice(&[]),
+        creator: Principal::from_slice(&[]),
+        name: "testing_name".to_string(),
+        description: "testing_desc".to_string(),
+        keywords: vec!["testing_keyword1".to_string(), "testing_keyword2".to_string()],
+        storage_type,
+    };
+
+    let create_args = Encode!(&video_info).expect("Could not encode video_info");
+
+    let create_response = actor.update_call("createVideo", create_args).await;
+
+    let result_id = match create_response {
+        Ok(result) => Decode!(result.as_slice(), VideoInfo).expect("Could not deduce video info from result").video_id.expect("Video Id was not send back"),
+        Err(err) => {
+            println!("Api Call error: {:?}", err);
+            return false;
+        },
+    };
+
+    let get_arg = Encode!(&result_id).expect("Could not encode video_id");
+
+    let get_response = actor.query_call("getVideoInfo", get_arg).await;
+
+    let get_result = match get_response {
+        Ok(result) => Decode!(result.as_slice(), WrappedVideoInfo).expect("Could not deduce video info from result").expect("Video was not in canister"),
+        Err(err) => {
+            println!("Api Call error: {:?}", err);
+            return false;
+        },
+    };
+
+    assert_eq!(result_id, get_result.video_id.expect("Video Id not in get_result"));
+    assert_eq!(video_info.storage_type, get_result.storage_type);
+    assert_eq!(video_info.keywords, get_result.keywords);
+    assert_eq!(video_info.description, get_result.description);
+    assert_eq!(video_info.name, get_result.name);
+
     return true;
 }
 
