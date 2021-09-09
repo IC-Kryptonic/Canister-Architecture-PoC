9549aeb9d6f27461a90c8224de5779b9b135012c
Alwin Stockinger <alwin.stockinger@tum.de>
2021-09-09 10:58:58 +0200

    ad canister init

---
+++ src/ad_manager/ad_canister/src/lib.rs
@@ -0,0 +1,35 @@
+use ic_cdk_macros::*;
+use ic_cdk::storage;
+use ic_cdk::export::Principal;
+
+pub type VideoId = String;
+pub type ChunkNum = usize;
+pub type Chunk = Vec<u8>;
+pub type Chunks = Vec<Chunk>;
+
+#[derive(CandidType, Deserialize)]
+pub struct AdInfo{
+    owner: Principal,
+    name: String,
+    chunk_num: ChunkNum,
+}
+
+
+///Creates a new ad video in the bucket
+#[update(name = "createAd")]
+pub async fn create_ad(){
+    unimplemented!()
+}
+
+///Inserts a new chunk
+#[update(name = "insertChunk")]
+pub async fn insert_chunk(chunk_num: ChunkNum, chunk: Chunk){
+    unimplemented!()
+}
+
+
+///Retrieve a chunk
+#[query(name = "getChunk")]
+pub async fn get_chunk(id: VideoId, chunk_num: ChunkNum) -> Option<&'static Chunk>{
+   unimplemented!()
+}
