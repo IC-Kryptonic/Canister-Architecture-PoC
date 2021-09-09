47950811d9402d0f57a665b7b48cc2e0a782af97
Alwin Stockinger <alwin.stockinger@tum.de>
2021-09-09 12:57:44 +0200

    fix: compiler warnings and errors

--- src/ad_manager/ad_canister/src/lib.rs
+++ src/ad_manager/ad_canister/src/lib.rs
@@ -7,29 +7,39 @@ pub type ChunkNum = usize;
 pub type Chunk = Vec<u8>;
 pub type Chunks = Vec<Chunk>;
 
-#[derive(CandidType, Deserialize)]
+#[derive(CandidType, Deserialize, Clone)]
 pub struct AdInfo{
     owner: Principal,
     name: String,
     chunk_num: ChunkNum,
 }
 
+impl Default for AdInfo {
+    fn default() -> Self {
+        return AdInfo{
+            owner: Principal::anonymous(),
+            name: String::from(""),
+            chunk_num: 0
+        }
+    }
+}
+
 
 ///Creates a new ad video in the bucket
 #[update(name = "createAd")]
-pub async fn create_ad(ad_info: &mut AdInfo){
-    let mut chunk_store = storage::get_mut::<Chunks>();
+pub async fn create_ad(mut ad_info: AdInfo){
+    let chunk_store = storage::get_mut::<Chunks>();
     let mut empty_chunks: Chunks = vec![Vec::new(); ad_info.chunk_num];
     chunk_store.append(&mut empty_chunks);
 
     let ad_info_store = storage::get_mut::<AdInfo>();
-    std::mem::swap(ad_info_store, ad_info);
+    std::mem::swap(ad_info_store, &mut ad_info);
 }
 
 ///Inserts a new chunk
 #[update(name = "insertChunk")]
 pub async fn insert_chunk(chunk_num: ChunkNum, chunk: Chunk){
-    let mut chunks = storage::get_mut::<Chunks>();
+    let chunks = storage::get_mut::<Chunks>();
 
     chunks[chunk_num] = chunk;
 }
@@ -38,5 +48,5 @@ pub async fn insert_chunk(chunk_num: ChunkNum, chunk: Chunk){
 ///Retrieve a chunk
 #[query(name = "getChunk")]
 pub async fn get_chunk(chunk_num: ChunkNum) -> Option<&'static Chunk>{
-    storage::get::<Chunk>().get(chunk_num)
+    storage::get::<Chunks>().get(chunk_num)
 }
