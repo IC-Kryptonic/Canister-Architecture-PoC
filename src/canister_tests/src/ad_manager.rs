26e7d7ee01d32191c96ec6ad0bc662bd6407d21c
Alwin Stockinger <alwin.stockinger@tum.de>
2021-09-12 13:26:15 +0200

    store ad test

--- src/canister_tests/src/ad_manager.rs
+++ src/canister_tests/src/ad_manager.rs
@@ -7,6 +7,7 @@ mod ad_manager_tests {
     use ic_agent::Identity;
 
     pub type ChunkNum = usize;
+    pub type Chunk = Vec<u8>;
 
     #[derive(CandidType, Deserialize)]
     pub struct AdInfo{
@@ -44,6 +45,39 @@ mod ad_manager_tests {
         assert!(result_ad.canister.is_some());
         assert_eq!(result_ad.owner, util::generate_pkcs8_identity(&util::PEKCS8_BYTES).sender().expect("Could net get sender from identity"));
         assert_eq!(result_ad.name, test_ad_info.name);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_store_ad() -> Result<(), String>{
+        let identity = util::generate_pkcs8_identity(&util::PEKCS8_BYTES);
+        let actor = Actor::from_name("ad_manager", identity).await;
+
+        let test_ad_info = create_test_ad();
+
+        let arg = Encode!(&test_ad_info).expect("Could not encode test_ad info");
+
+        let response = actor.update_call("createAd", arg).await;
+
+        let raw_result = util::check_ok(response);
+
+        let result_ad = Decode!(raw_result.as_slice(), AdInfo).expect("Could not decode result ad");
+
+        let ad_canister = result_ad.canister.expect("No canister principal in result ad");
+        let ad_data: [u8; 3] = [0xCA, 0xFF, 0xEE];
+        let chunk_num: u64 = 0;
+        let data_arg = Encode!(&chunk_num, &ad_data).expect("Could not encode ad_data");
+
+        let ad_actor = Actor{
+            agent: actor.agent,
+            principal: ad_canister,
+        };
+
+        let insert_response = ad_actor.update_call("insertChunk", data_arg).await;
+        let raw_result = util::check_ok(insert_response);
+
+        Decode!(raw_result.as_slice(), ()).expect("Could not decode insert response result");
 
         Ok(())
     }
