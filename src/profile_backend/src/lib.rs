991e516b9e6b05f62ea178b4b59983ef471b1be4
Alwin Stockinger <alwin.stockinger@tum.de>
2021-09-30 10:42:50 +0200

    take principal by referece for check_profile, since ownership is only requiered if there is no profile yet

--- src/profile_backend/src/lib.rs
+++ src/profile_backend/src/lib.rs
@@ -11,7 +11,7 @@ pub type ProfileStore = HashMap<Principal, Profile>;
 ///This function returns a Profile if there is one for the principal, otherwise it returns creates a new one just with the principal and the name as an empty string.
 #[query(name = "get_profile")]
 pub fn get_profile(id: Principal) -> &'static Profile {
-    check_profile(id.clone());
+    check_profile(&id);
 
     return &storage::get::<ProfileStore>()[&id];
 }
@@ -43,7 +43,7 @@ pub fn put_profile(mut profile: Profile){
 #[update(name = "add_like")]
 pub fn add_like(video_princ: Principal){
     let caller = ic_cdk::caller();
-    check_profile(caller);
+    check_profile(&caller);
 
     let profile_store = storage::get_mut::<ProfileStore>();
     profile_store.get_mut(&caller).expect("Profile not stored").likes.insert(video_princ);
@@ -53,7 +53,7 @@ pub fn add_like(video_princ: Principal){
 #[update(name = "add_comment")]
 pub fn add_comment(video_princ: Principal){
     let caller = ic_cdk::caller();
-    check_profile(caller);
+    check_profile(&caller);
 
     let profile_store = storage::get_mut::<ProfileStore>();
     profile_store.get_mut(&caller).expect("Profile not stored").comments.insert(video_princ);
@@ -63,21 +63,21 @@ pub fn add_comment(video_princ: Principal){
 #[update(name = "add_view")]
 pub fn add_view(video_princ: Principal){
     let caller = ic_cdk::caller();
-    check_profile(caller);
+    check_profile(&caller);
 
     let profile_store = storage::get_mut::<ProfileStore>();
     profile_store.get_mut(&caller).expect("Profile not stored").viewed.insert(video_princ);
 }
 
-fn check_profile(princ: Principal){
+fn check_profile(princ: &Principal){
     let profile_store = storage::get_mut::<ProfileStore>();
 
-    if !profile_store.contains_key(&princ) {
+    if !profile_store.contains_key(princ) {
 
         profile_store.insert(
             princ.clone(),
             Profile {
-                principal: princ,
+                principal: princ.clone(),
                 name: "anon".to_string(),
                 likes: Default::default(),
                 comments: Default::default(),
